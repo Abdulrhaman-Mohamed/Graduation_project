04f33ec4f74ea1801d5f090075619eb3cf0ebdd8
Baher <ferventfrost5@gmail.com>
2023-05-05 01:30:17 +0300

    SocketsHandlerClass is semi-implemented

---
+++ Repo_Core/Interface/ISocketsHandler.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.WebSockets;
+
+namespace Repo_Core.Interface
+{
+    public enum Sockets
+    {
+        BinarySocket,
+        DataSocket,
+        ControllerSokcet
+    }
+
+    public enum SocketsType
+    {
+        Front,
+        Rover
+    }
+
+    public interface ISocketsHandler
+    {
+        public void SetSocket(WebSocket webSocket, Sockets sockets, SocketsType type);
+        public void ReleaseSocket(Sockets sockets, SocketsType type);
+
+        //Note Null Sockets situation isn't handled ( Meaning no socket exit at call time is may give error)
+        // Accept Data is data from Front to Rover
+        public Task AcceptData(byte[] bytes, Sockets sockets);
+        // Send Data is data from Rover to Front
+        public Task SendData(byte[] bytes, Sockets sockets);
+
+    }
+}
---
+++ Repo_EF/Repo_Method/SocketsHandler.cs
@@ -0,0 +1,48 @@
+﻿using Repo_Core.Interface;
+using System.Net.WebSockets;
+
+namespace Repo_EF.Repo_Method
+{
+    public class SocketsHandler : ISocketsHandler
+    {
+        private WebSocket[] FrontSockets = new WebSocket[3];
+        private WebSocket[] RoverSockets = new WebSocket[3];
+
+        protected async Task HandleData() { }
+
+        public void SetSocket(WebSocket webSocket, Sockets socket, SocketsType type) 
+        {
+            if(type == SocketsType.Front)
+                FrontSockets[(int)socket] = webSocket;
+            else
+                RoverSockets[(int)socket] = webSocket;
+        }
+        public void ReleaseSocket(Sockets socket, SocketsType type) 
+        {
+            if(type == SocketsType.Front)
+                FrontSockets[(int)socket].Dispose();
+            else
+                RoverSockets[(int)socket].Dispose();
+        }
+
+        public async Task AcceptData(byte[] bytes, Sockets sockets ) 
+        {
+            await RoverSockets[(int)sockets].SendAsync(
+                new ArraySegment<byte>(bytes,0,bytes.Length),
+                WebSocketMessageType.Binary,
+                true,
+                CancellationToken.None
+                );
+        }
+        public async Task SendData(byte[] bytes, Sockets sockets) 
+        {
+            await FrontSockets[(int)sockets].SendAsync(
+                new ArraySegment<byte>(bytes, 0, bytes.Length),
+                WebSocketMessageType.Binary,
+                true,
+                CancellationToken.None
+                );
+        }
+
+    }
+}
