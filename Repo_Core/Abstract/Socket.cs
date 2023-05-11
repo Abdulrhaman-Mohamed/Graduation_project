e3ba8c55fe82449a2802bfc6712e70e33e16bf9d
Baher <ferventfrost5@gmail.com>
2023-05-11 16:50:14 +0300

    Change abstract method structure and and some struct, it can be removed later

--- Repo_Core/Abstract/Socket.cs
+++ Repo_Core/Abstract/Socket.cs
@@ -1,32 +1,131 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
+
+
+struct AESData
+{
+    byte[] IV;
+    byte[] EncryptedData;
+}
+enum AckType
+{
+    StartSetup,
+    EndSetup,
+    SendTelemetry,
+    EndTelemetry,
+    SendPlan,
+    EndPlan,
+    PlanReceived,
+    PlanDone,
+    PlanReSend,
+    StratOnline,
+    EndOnline
+};
+
+enum FrameType
+{
+    Plan,
+    Command,
+    Request,
+    Data,
+    Image
+};
+
+struct Header
+{
+    FrameType Type;
+    byte[] IV;
+    int CRC;
+    int FrameLength;
+}
+
+struct PlanBody
+{
+    int NumberofPlans;
+    int NumberofFrames;
+    long Time;
+}
+
+struct CommandBody
+{
+    int PlanID;
+    int SequenceID;
+    int SubSystemID;
+    int CommandID;
+    int Delay;
+    int CommandRepeat;
+}
+
+struct RequestBody
+{
+    bool ACK;
+    AckType Type;
+}
+
+struct RoverData
+{
+    int PlanID;
+    int SequenceID;
+    long Time;
+}
+
+struct ImageData
+{
+    int PlanID;
+    int SequenceID;
+    long Time;
+}
+
+struct SensorReadings
+{
+    int X;
+    int Y;
+    int Z;
+}
+
+// Use Bitmap to accept array of bytes
+/* Note Chatgpt responded to me with this when i asked "how to accept array of byte that represent image in struct"
+Note that using a struct to hold large arrays of data, such as image data, may not be the most efficient approach. 
+It is generally more efficient to work with arrays directly or use dedicated classes like Bitmap for image manipulation. 
+Structs are typically used for small, lightweight data structures. 
+Consider your specific requirements and performance considerations when deciding on the appropriate approach.
+*/
+
 
 namespace Repo_Core.Abstract
 {
     abstract class Socket
     {
         // Data Types and Parameter will change.
-        protected byte[] IV = new byte[16];
-        public abstract void SetSocket();
-        public abstract void CloseSocket();
-        public abstract void Serialiazation();
-        public abstract void Deserialiazation();
+
+        public abstract byte[] SerialiazationHeader(Header header);
+        public abstract byte[] SerialiazationPlan(PlanBody plan);
+        public abstract byte[] SerialiazationCommand(CommandBody command);
+        public abstract byte[] SerialiazationRequests(RequestBody request);
+
+        public abstract Header DeserialiazationHeader(byte[] bytes);
+        public abstract SensorReadings DeserialiazationData(byte[] bytes);
+        // this well only return (PlanID, SequenceID, Time) you can discard this method or separate image from reset of the request
+        public abstract ImageData DeserialiazationImage(byte[] bytes);
+        public abstract RequestBody DeserialiazationResponse(byte[] bytes);
+
+
+        public abstract void SetSocket(WebSocket webSocket, int SocketID);
+        public abstract void CloseSocket(int SocketID);
         public abstract void Run();
-        public abstract void SendBytes();
-        public abstract void AcceptBytes();
-        public byte[] GetIV()
+        public abstract Task SendBytes(byte[] Buffer);
+        public abstract Task AcceptBytes(byte[] Buffer);
+
+        public AESData AESEncryption(byte[] Data)
         {
-            byte[] IV = new byte[16];
-            return IV;
+            AESData aes = new AESData();
+            return aes;
         }
-        public string AESEncryption(byte[] Data)
-        {
-            return "";
-        }
-        public byte[] AESDecryption(string Data)
+        public byte[] AESDecryption(AESData Data)
         {
             byte[] data = new byte[50];
             return data;
